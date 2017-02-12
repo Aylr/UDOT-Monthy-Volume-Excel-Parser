c1ce56cd39b0b970d7c06f7b64defaada4d6bb65
Aylr <taylor@00.local>
2017-02-11 19:01:57 -0700

    * made a nice debugging list printer

--- monthly_volume_excel_parser.py
+++ monthly_volume_excel_parser.py
@@ -57,7 +57,7 @@ def is_cell_value_string_or_unicode(value):
         return False
 
 
-def get_data_adjacent_to_target_lable(row, target_string=SITE_NAME):
+def get_data_adjacent_to_target_label(row, target_string=SITE_NAME):
     # type: (object, str) -> object
     result = None
 
@@ -80,7 +80,9 @@ def get_data_adjacent_to_target_lable(row, target_string=SITE_NAME):
 
 
 def find_header_rows(sheet):
+    # type: (object) -> List[object]
     header_rows = []
+
     for row in sheet.get_rows():
         if does_row_contain_string(row, REPORT_TYPE_HEADER):
             header_rows.append(row)
@@ -89,21 +91,30 @@ def find_header_rows(sheet):
 
 
 def find_site_names(sheet):
+    # type: (object) -> List[object]
     site_names = []
+
     for row in sheet.get_rows():
         if does_row_contain_string(row, SITE_NAME):
-            site_names.append(get_data_adjacent_to_target_lable(row, SITE_NAME))
+            site_names.append(get_data_adjacent_to_target_label(row, SITE_NAME))
 
     return site_names
 
 
 def find_locations(sheet):
+    # type: (object) -> List[object]
     site_locations = []
+
     for row in sheet.get_rows():
         if does_row_contain_string(row, SITE_LOCATION):
-            site_locations.append(get_data_adjacent_to_target_lable(row, SITE_LOCATION))
+            site_locations.append(get_data_adjacent_to_target_label(row, SITE_LOCATION))
 
     return site_locations
+
+
+def nice_list_print(list):
+    for i, item in enumerate(list):
+        print '{0}: {1}'.format(i, item)
 
 
 def main():
@@ -115,17 +126,17 @@ def main():
 
     header_rows = find_header_rows(sheet)
     print '----------------- Found {0} header rows -----------------'.format(len(header_rows))
-    print header_rows
+    nice_list_print(header_rows)
     print
 
     site_names = find_site_names(sheet)
     print '----------------- Found {0} site names -----------------'.format(len(site_names))
-    print site_names
+    nice_list_print(site_names)
     print
 
     site_locations = find_locations(sheet)
     print '----------------- Found {0} site locations -----------------'.format(len(site_locations))
-    print site_locations
+    nice_list_print(site_locations)
     print
 
 
