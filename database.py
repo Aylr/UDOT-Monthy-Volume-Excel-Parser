6e1615a30a59155b3a0e61b9bbc0cd04d16e3fd4
Aylr <taylor@00.local>
2017-02-20 21:00:01 -0700

    * changed to guard clauses

--- database.py
+++ database.py
@@ -32,11 +32,13 @@ def get_all_records_from_table(db, table_name):
 
 
 def recreate_tables(db):
-    if type(db) is records.Database:
-        db.query('DROP TABLE IF EXISTS {};'.format(SITE_TABLE))
-        db.query('DROP TABLE IF EXISTS {};'.format(VOLUME_TABLE))
-        db.query('CREATE TABLE {} (id INTEGER PRIMARY KEY AUTOINCREMENT, site_name TEXT, site_location TEXT);'.format(SITE_TABLE))
-        db.query('CREATE TABLE {} (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp INT, volume INT, site_id INT, FOREIGN KEY (site_id) REFERENCES sites(id));'.format(VOLUME_TABLE))
+    if type(db) is not records.Database:
+        return
+
+    db.query('DROP TABLE IF EXISTS {};'.format(SITE_TABLE))
+    db.query('DROP TABLE IF EXISTS {};'.format(VOLUME_TABLE))
+    db.query('CREATE TABLE {} (id INTEGER PRIMARY KEY AUTOINCREMENT, site_name TEXT, site_location TEXT);'.format(SITE_TABLE))
+    db.query('CREATE TABLE {} (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp INT, volume INT, site_id INT, FOREIGN KEY (site_id) REFERENCES sites(id));'.format(VOLUME_TABLE))
 
 
 def insert_site(db, site_name, site_location):
@@ -50,11 +52,13 @@ def insert_site(db, site_name, site_location):
 
 
 def does_site_exist(db, site_name):
-    if type(db) is records.Database:
-        result = db.query('SELECT * FROM ' + SITE_TABLE + ' WHERE site_name = :site_name;', site_name=site_name)
-        for row in result:
-            print row
-        return len(result.all()) is not 0
+    if type(db) is not records.Database:
+        return
+
+    result = db.query('SELECT * FROM ' + SITE_TABLE + ' WHERE site_name = :site_name;', site_name=site_name)
+    for row in result:
+        print row
+    return len(result.all()) is not 0
 
 
 def main():
@@ -62,12 +66,11 @@ def main():
     # db = setup_db_client('udot.db')
     # recreate_tables(db)
     db = setup_db_client('udot.db')
+
     insert_site(db, 'stuff', 'stuff')
     insert_site(db, 'stuff2', 'stuff2')
-    x = does_site_exist(db, 'stuff')
 
-    print x
-
+    print does_site_exist(db, 'stuff')
 
 
 if __name__  == '__main__':
